e91cbc05ef921d525b4ac241460551a23df7d3c8
nathantotten <74629300+nathantotten@users.noreply.github.com>
2021-10-06 16:29:14 +0100

    Fixed project 4, practical 2

--- Pointer4/test4.cpp
+++ Pointer4/test4.cpp
@@ -3,28 +3,24 @@
 
 using namespace std;
 
-int search(char* pchs, int& size, char key)
+int search(char* pchs, int size, char key)
 {
 	// initialise count variable
 	int count = 0;
 
-	// allocate char array using provided size
-	//char myArray[] = { 0 };
-	char* myArray = new char[size];
-	srand(time(NULL));
-	// for each element in array, assign a random char using ascii 97 + rand() % 26.
 	// use size to loop through each item in the character array
-	for (int i = 0; i < size; i++) {
-		myArray[i] = 97 + rand() % 26;
-		//cout << myArray[i] << endl;
-		// using pointer arithmetic, compare the character currently being pointed to with the key char
-		// if value at pchs matches value of key then increase count by 1
-		if ( myArray[i] == key) {
-			count++;
+	if (pchs != NULL) {
+		for (int i = 0; i < size; i++) {
+			// using pointer arithmetic, compare the character currently being pointed to with the key char
+			// if value at pchs matches value of key then increase count by 1
+			if (pchs[i] == key) {
+				count++;
+			}
 		}
+
 	}
 
-	delete[] myArray;
+	delete[] pchs;
 	return count;
 }
 
@@ -44,7 +40,20 @@ int main()
 	cin >> c;
 	cout << "The key is: " << c << endl;
 
-	count = search(NULL, arraySize, c);
+	srand(time(NULL));
+
+	// allocate an array using provided size.
+	char* myArray = new char[arraySize];
+
+	// for each element in array, assign a random char using ascii 97 + rand() % 26.
+	for (int i = 0; i < arraySize; i++) {
+		myArray[i] = 97 + rand() % 26;
+		// print char to test it a little.
+		//cout << myArray[i] << endl;
+	}
+
+
+	count = search(myArray, arraySize, c);
 	cout << "Number of times key appears in array is: " << count << endl;
 
 	return 0;
